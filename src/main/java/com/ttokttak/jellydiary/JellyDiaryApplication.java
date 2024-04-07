9eb22eabb4692d1f97534b7ef9852e0dcba4a621
Lee-Siny <sinyoung.siny@gmail.com>
2024-04-07 20:59:17 +0900

    Initial commit

---
+++ src/main/java/com/ttokttak/jellydiary/JellyDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.ttokttak.jellydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JellyDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttokttak/jellydiary/JellyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttokttak.jellydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
